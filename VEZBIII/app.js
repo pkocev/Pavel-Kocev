27f0a8d956611cc6886f7ce2bae2f4b4511107a5
pkocev <119971181+pkocev@users.noreply.github.com>
2022-12-24 15:26:58 +0100

    vezbi 24.12

---
+++ VEZBIII/app.js
@@ -0,0 +1,73 @@
+// EXERCICE 1
+// function difference(num) {
+
+//     if (num > 13) {
+//         num = num * 2
+//     }
+//     console.log(num)
+// }
+// difference(14)
+
+
+// function closeTo100(numberOne,numberTwo){
+//     if(numberOne>numberTwo){
+//         return `${numberOne} is closer to 100`;
+//     } else {
+//         return `${numberTwo} is closer to 100`;
+//     }
+// }
+
+// console.log(closeTo100(50,300))
+
+// EXERCICE 2
+
+// function near_100(numberOne,numberTwo) {
+//     if (numberOne != numberTwo)
+//     {
+//     numOne = Math.abs(numberOne - 100);
+//     numTwo = Math.abs(numberTwo - 100);
+
+//     if (numOne < numTwo)
+//     {
+//       return `${numberOne} is closer to 100`;
+//     }
+//     if (numTwo < numOne)
+//     {
+//       return `${numberTwo} is closer to 100`;
+//     }
+//     return 0;
+// }
+// else
+//   return false;
+// }
+
+//   console.log(near_100(-200,40));
+
+
+// EXERCICE 3
+// function calculateExpenses(salary,rent,bills){
+//     let left=(salary-rent-bills)
+//     let total=(rent+bills)
+//     return ` Bob has ${left} eur left and his total expenses are ${total} eur` 
+
+
+// }
+
+// console.log(calculateExpenses(1000,375,250))
+
+// // EXERCICE 4
+
+// const grades = [10, 6, 8, 9, 6]
+// console.log(grades)
+// let total = 0
+// function calculateAverage() {
+//     for (i = 0; i < grades.length; i++) {
+//         total += grades[i]
+
+//     }
+//     const avg = total/grades.length
+//     return `The averege is ${avg}`
+// }
+// console.log(calculateAverage())
+
+
